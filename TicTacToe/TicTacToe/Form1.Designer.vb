2ad7e676138e319d5ba6024eeed6ac6038ad435a
Colten.Badgley.SD23 <colten.badgley@sd23.bc.ca>
2018-02-19 13:59:21 -0800

    Labels

--- TicTacToe/TicTacToe/Form1.Designer.vb
+++ TicTacToe/TicTacToe/Form1.Designer.vb
@@ -25,16 +25,17 @@ Partial Class Form1
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button10 = New System.Windows.Forms.Button()
+        Me.startgame = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
         Me.Button6 = New System.Windows.Forms.Button()
-        Me.Button7 = New System.Windows.Forms.Button()
-        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
         Me.Button9 = New System.Windows.Forms.Button()
-        Me.playerdisplay = New System.Windows.Forms.Label()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.restartbutton = New System.Windows.Forms.Button()
+        Me.playerdisplay = New System.Windows.Forms.TextBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -51,32 +52,34 @@ Partial Class Form1
         Me.Button1.BackColor = System.Drawing.Color.Transparent
         Me.Button1.FlatAppearance.BorderSize = 0
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.Location = New System.Drawing.Point(13, 25)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(152, 152)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "X"
         Me.Button1.UseVisualStyleBackColor = False
         '
-        'Button10
+        'startgame
         '
-        Me.Button10.Location = New System.Drawing.Point(13, 2)
-        Me.Button10.Name = "Button10"
-        Me.Button10.Size = New System.Drawing.Size(82, 23)
-        Me.Button10.TabIndex = 10
-        Me.Button10.Text = "Start Game"
-        Me.Button10.UseVisualStyleBackColor = True
+        Me.startgame.Location = New System.Drawing.Point(13, 2)
+        Me.startgame.Name = "startgame"
+        Me.startgame.Size = New System.Drawing.Size(82, 23)
+        Me.startgame.TabIndex = 10
+        Me.startgame.Text = "Start Game"
+        Me.startgame.UseVisualStyleBackColor = True
         '
         'Button2
         '
         Me.Button2.BackColor = System.Drawing.Color.Transparent
         Me.Button2.FlatAppearance.BorderSize = 0
         Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button2.Location = New System.Drawing.Point(166, 25)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(152, 152)
         Me.Button2.TabIndex = 11
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "O"
         Me.Button2.UseVisualStyleBackColor = False
         '
         'Button3
@@ -84,94 +87,112 @@ Partial Class Form1
         Me.Button3.BackColor = System.Drawing.Color.Transparent
         Me.Button3.FlatAppearance.BorderSize = 0
         Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button3.Location = New System.Drawing.Point(320, 25)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(152, 152)
         Me.Button3.TabIndex = 12
-        Me.Button3.Text = "Button3"
+        Me.Button3.Text = "X"
         Me.Button3.UseVisualStyleBackColor = False
-        '
-        'Button4
-        '
-        Me.Button4.BackColor = System.Drawing.Color.Transparent
-        Me.Button4.FlatAppearance.BorderSize = 0
-        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button4.Location = New System.Drawing.Point(320, 182)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(152, 152)
-        Me.Button4.TabIndex = 15
-        Me.Button4.Text = "Button4"
-        Me.Button4.UseVisualStyleBackColor = False
-        '
-        'Button5
-        '
-        Me.Button5.BackColor = System.Drawing.Color.Transparent
-        Me.Button5.FlatAppearance.BorderSize = 0
-        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button5.Location = New System.Drawing.Point(166, 182)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(152, 152)
-        Me.Button5.TabIndex = 14
-        Me.Button5.Text = "Button5"
-        Me.Button5.UseVisualStyleBackColor = False
         '
         'Button6
         '
         Me.Button6.BackColor = System.Drawing.Color.Transparent
         Me.Button6.FlatAppearance.BorderSize = 0
         Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button6.Location = New System.Drawing.Point(13, 182)
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(320, 182)
         Me.Button6.Name = "Button6"
         Me.Button6.Size = New System.Drawing.Size(152, 152)
-        Me.Button6.TabIndex = 13
-        Me.Button6.Text = "Button4"
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "O"
         Me.Button6.UseVisualStyleBackColor = False
         '
-        'Button7
+        'Button5
         '
-        Me.Button7.BackColor = System.Drawing.Color.Transparent
-        Me.Button7.FlatAppearance.BorderSize = 0
-        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button7.Location = New System.Drawing.Point(320, 336)
-        Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(152, 152)
-        Me.Button7.TabIndex = 18
-        Me.Button7.Text = "Button7"
-        Me.Button7.UseVisualStyleBackColor = False
+        Me.Button5.BackColor = System.Drawing.Color.Transparent
+        Me.Button5.FlatAppearance.BorderSize = 0
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(166, 182)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(152, 152)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "X"
+        Me.Button5.UseVisualStyleBackColor = False
         '
-        'Button8
+        'Button4
         '
-        Me.Button8.BackColor = System.Drawing.Color.Transparent
-        Me.Button8.FlatAppearance.BorderSize = 0
-        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button8.Location = New System.Drawing.Point(166, 336)
-        Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(152, 152)
-        Me.Button8.TabIndex = 17
-        Me.Button8.Text = "Button8"
-        Me.Button8.UseVisualStyleBackColor = False
+        Me.Button4.BackColor = System.Drawing.Color.Transparent
+        Me.Button4.FlatAppearance.BorderSize = 0
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(13, 182)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(152, 152)
+        Me.Button4.TabIndex = 13
+        Me.Button4.Text = "O"
+        Me.Button4.UseVisualStyleBackColor = False
         '
         'Button9
         '
         Me.Button9.BackColor = System.Drawing.Color.Transparent
         Me.Button9.FlatAppearance.BorderSize = 0
         Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button9.Location = New System.Drawing.Point(12, 337)
+        Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(320, 336)
         Me.Button9.Name = "Button9"
         Me.Button9.Size = New System.Drawing.Size(152, 152)
-        Me.Button9.TabIndex = 16
-        Me.Button9.Text = "Button9"
+        Me.Button9.TabIndex = 18
+        Me.Button9.Text = "X"
         Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.Transparent
+        Me.Button8.FlatAppearance.BorderSize = 0
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(166, 336)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(152, 152)
+        Me.Button8.TabIndex = 17
+        Me.Button8.Text = "O"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.Transparent
+        Me.Button7.FlatAppearance.BorderSize = 0
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(12, 337)
+        Me.Button7.Name = "Button7"
+        Me.Button7.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Button7.Size = New System.Drawing.Size(152, 152)
+        Me.Button7.TabIndex = 16
+        Me.Button7.Text = "X"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'restartbutton
+        '
+        Me.restartbutton.Location = New System.Drawing.Point(390, 2)
+        Me.restartbutton.Name = "restartbutton"
+        Me.restartbutton.Size = New System.Drawing.Size(82, 23)
+        Me.restartbutton.TabIndex = 20
+        Me.restartbutton.Text = "Restart"
+        Me.restartbutton.UseVisualStyleBackColor = True
         '
         'playerdisplay
         '
-        Me.playerdisplay.AutoSize = True
-        Me.playerdisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.playerdisplay.Location = New System.Drawing.Point(215, 2)
+        Me.playerdisplay.Enabled = False
+        Me.playerdisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.playerdisplay.Location = New System.Drawing.Point(196, 3)
         Me.playerdisplay.Name = "playerdisplay"
-        Me.playerdisplay.Size = New System.Drawing.Size(58, 20)
-        Me.playerdisplay.TabIndex = 19
+        Me.playerdisplay.Size = New System.Drawing.Size(100, 22)
+        Me.playerdisplay.TabIndex = 21
         Me.playerdisplay.Text = "Player"
+        Me.playerdisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'Form1
         '
@@ -179,15 +200,16 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(484, 500)
         Me.Controls.Add(Me.playerdisplay)
-        Me.Controls.Add(Me.Button7)
-        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.restartbutton)
         Me.Controls.Add(Me.Button9)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
         Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.startgame)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.PictureBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
@@ -203,14 +225,15 @@ Partial Class Form1
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Button1 As Button
-    Friend WithEvents Button10 As Button
+    Friend WithEvents startgame As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents Button5 As Button
     Friend WithEvents Button6 As Button
-    Friend WithEvents Button7 As Button
-    Friend WithEvents Button8 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
     Friend WithEvents Button9 As Button
-    Friend WithEvents playerdisplay As Label
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents restartbutton As Button
+    Friend WithEvents playerdisplay As TextBox
 End Class
--- TicTacToe/TicTacToe/Form1.vb
+++ TicTacToe/TicTacToe/Form1.vb
@@ -1,5 +1,8 @@
 ﻿Public Class Form1
     Dim footlettuce(2, 2) As Byte
+    Dim currentTurn As Boolean = False
+    'false is Player 1 or x
+    'true is Player 2 or o
     Private Sub PictureBox1_Paint(sender As Object, e As PaintEventArgs) Handles PictureBox1.Paint
         e.Graphics.DrawLine(Pens.Black, 153, 0, 153, 460)
         e.Graphics.DrawLine(Pens.Black, 306, 0, 306, 460)
@@ -7,8 +10,53 @@
         e.Graphics.DrawLine(Pens.Black, 0, 306, 460, 306)
     End Sub
 
-    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        footlettuce(0, 0) = 1
 
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        footlettuce(0, 1) = 1
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        footlettuce(0, 2) = 1
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        footlettuce(1, 0) = 1
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        footlettuce(1, 1) = 1
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        footlettuce(1, 2) = 1
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        footlettuce(2, 0) = 1
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        footlettuce(2, 1) = 1
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        footlettuce(2, 2) = 1
+    End Sub
+    Sub chooselettuce(ByVal boi As Object)
+        If currentTurn = False Then
+            boi.text = "X"
+            playerdisplay.Text = "O's Turn"
+            Cursor = Cursors.WaitCursor
+        Else
+            boi.text = "O"
+            playerdisplay.Text = "X's Turn"
+            Cursor = Cursors.Cross
+        End If
+    End Sub
 End Class
+
 
